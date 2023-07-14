8b4b8c70c87bb4ffcb6447024e930af366db966e
William Nguyen <williamnguyen773@gmail.com>
2023-07-14 14:15:53 -0500

    birds component complete

--- src/components/Birds.jsx
+++ src/components/Birds.jsx
@@ -20,22 +20,45 @@ const Birds = () => {
     fetchBird();
   }, []);
 
+  let attributionText = bird?.attribute || '';
+  let attributionLink = '';
+  let ccVersion = '';
+
+  if (bird?.attribute?.includes('<')) {
+    const attribution = bird.attribute.split('<');
+    attributionLink = attribution[1]?.split('>')[0];
+    const ccParts = attribution[0].split(/CC BY-SA|CC BY/);
+    attributionText = ccParts[0];
+    ccVersion = ccParts[ccParts.length - 1].trim();
+  }
+
   if (error) return <div>Error loading bird data.</div>;
   if (!bird) return <div>Loading...</div>;
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-4'>
-      {bird.image_url && (
-        <img
-          className='object-contain h-64 sm:h-96 w-full block mt-2 sm:mt-4'
-          src={bird.image_url}
-          alt={bird.name}
-        />
-      )}
+      <img
+        className='object-contain h-64 sm:h-96 w-full block mt-2 sm:mt-4'
+        src={bird.image_url}
+        alt={bird.name}
+      />
       <div className='mt-2 sm:mt-4 bg-white shadow-lg rounded-lg overflow-hidden p-4 max-w-xl w-full'>
-        <h2>{bird.name}</h2>
+        <h2 className='font-bold'>{bird.name}</h2>
         <p>{bird.description}</p>
-        <p>{bird.attribute}</p>
+        <p>
+          {attributionText || 'No attribution information available'}
+          {attributionLink && (
+            <a
+              className='text-blue-500 hover:text-blue-800'
+              href={attributionLink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              CC BY{ccVersion && '-'}
+              {ccVersion}
+            </a>
+          )}
+        </p>
       </div>
       <button
         className='mt-4 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
