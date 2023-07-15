103a23e874657d84b1b89de58737a75c5e4c1922
William Nguyen <williamnguyen773@gmail.com>
2023-07-15 13:14:25 -0500

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='flex flex-col sm:flex-row items-center justify-center p-4 bg-gray-700 text-white'>
+      <p className='text-center sm:text-left'>
+        &copy; 2023 Birds of Surprise. All rights reserved. Website design and
+        layout are the property of the owner.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
